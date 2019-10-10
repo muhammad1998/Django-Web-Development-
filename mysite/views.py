b2ce5d670203f23d872f7e0ee44163f68406ec36
muhammad1998 <m.basithussain25@gmail.com>
2019-10-10 13:19:46 +0500

    added in views.py punctuation remove webpage

--- mysite/views.py
+++ mysite/views.py
@@ -8,7 +8,17 @@ def index(request):
     #return HttpResponse('''<a href="https://www.youtube.com/watch?v=AepgWsROO4k&list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9&index=7">click me</a>''')
 def analyzer(request):
     djtext = request.GET.get('text','default')
-    djcheck = request.GET.get('check','default')
-    print(djtext[0])
-    param = {'purpose':'Remove Punctuation','analyzed_text':djtext}
+    djcheck = request.GET.get('check','off')
+    if djcheck == "on":
+        analyzer_text = ""
+        Punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        for char in djtext:
+            if char not in Punctuations:
+                analyzer_text = analyzer_text+char
+    
+
+        param = {'purpose':'Remove Punctuation','analyzed_text':analyzer_text}
+    else:
+        param = {'purpose':'Remove Punctuation','analyzed_text':djtext}
+        
     return render(request,'about.html',param)
